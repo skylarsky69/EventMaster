465e63fc1453478b6aa8b91d171d1441eaa5c068
Nikolai Penev <Nikolai Penev@DESKTOP-6D8U1UG>
2026-03-06 17:20:28 +0200

    Add project files.

---
+++ EventMaster.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EventMaster.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
