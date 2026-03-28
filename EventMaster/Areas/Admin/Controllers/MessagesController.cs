f7c99174084d9a8a4591b4fd7fad7dadc8e193f6
Nikolai Penev <Nikolai Penev@DESKTOP-6D8U1UG>
2026-03-29 01:43:35 +0200

    Add Admin Messages management and update navigation

---
+++ EventMaster/Areas/Admin/Controllers/MessagesController.cs
@@ -0,0 +1,42 @@
+﻿using EventMaster.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventMaster.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Administrator")]
+    public class MessagesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MessagesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Списък с всички съобщения
+        public async Task<IActionResult> Index()
+        {
+            var messages = await _context.ContactMessages
+                .OrderByDescending(m => m.SentOn)
+                .ToListAsync();
+            return View(messages);
+        }
+
+        // Изтриване на съобщение
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var message = await _context.ContactMessages.FindAsync(id);
+            if (message != null)
+            {
+                _context.ContactMessages.Remove(message);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
