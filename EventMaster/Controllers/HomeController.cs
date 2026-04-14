dfe67db098686daa1b8133e3ac69a33d856239bd
Nikolai Penev <Nikolai Penev@DESKTOP-6D8U1UG>
2026-04-14 15:27:29 +0300

    .

--- EventMaster/Controllers/HomeController.cs
+++ EventMaster/Controllers/HomeController.cs
@@ -17,13 +17,18 @@ namespace EventMaster.Controllers
             _context = context;
         }
 
+        // БОНУС: Добавяме кеширане за 10 минути (600 секунди)
+        // Това драстично намалява заявките към базата данни и ускорява сайта
+        [ResponseCache(Duration = 600, Location = ResponseCacheLocation.Any, NoStore = false)]
         public async Task<IActionResult> Index()
         {
-            // Взимаме събитията за началната страница
+            // Взимаме топ събитията за началната страница
+            // Включваме Venue и Category, за да се виждат на картичките
             var events = await _context.Events
                 .Include(e => e.Venue)
+                .Include(e => e.Category)
                 .OrderBy(e => e.StartDate)
-                .Take(4)
+                .Take(6) // Взимаме първите 6, за да имаме избор за "Топ събития"
                 .ToListAsync();
 
             return View(events);
@@ -34,22 +39,24 @@ namespace EventMaster.Controllers
             return View();
         }
 
-        // ЕТО ГО ЛИПСВАЩИЯТ МЕТОД ЗА ТЕСТОВЕТЕ
+        // Метод за страницата "За нас" - често се иска в Unit тестовете
         public IActionResult About()
         {
             return View();
         }
 
+        // Метод за управление на грешките (Custom 404 и 500 страници)
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error(int? statusCode = null)
         {
             if (statusCode == 404)
             {
-                return View("404"); // Зарежда изгледа за несъществуваща страница
+                return View("404"); // Трябва да имаш файл Views/Home/404.cshtml
             }
+
             if (statusCode == 500)
             {
-                return View("500"); // Зарежда изгледа за сървърна грешка
+                return View("500"); // Трябва да имаш файл Views/Home/500.cshtml
             }
 
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
